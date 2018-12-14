1b8ba4e22fddace418a9f59b5fdcd3b777f52619
Lucas Moten <lucas.moten@deciphernow.com>
2018-12-14 06:40:12 -0500

    Adding docker artifacts

---
+++ services/dice/build_docker_iamge.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+# assume we are in the service directory
+SERVICEDIR=$PWD
+DOCKERDIR="${SERVICEDIR}/docker"
+BUILDFILES="${DOCKERDIR}/build"
+
+mkdir -p "$BUILDFILES"
+
+(
+	cd "cmd/server"
+	CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -a -tags netgo -ldflags '-w' -o "$BUILDFILES/dice" .
+)
+
+(
+	cp "settings.toml" "$BUILDFILES/."
+	#cd $DOCKERDIR
+	docker build -t deciphernow/dice:latest .
+	rm -rf $BUILDFILES
+)
+
